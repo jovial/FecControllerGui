afccf2b74aeead1f5e96b071393fd688e7656c81
Will Szumski <will@cowboycoders.org>
2017-02-24 03:17:20 +0000

    a few small steps in the right direction

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 /**
-  * Created by fluxoid on 17/02/17.
-  */
+ * Created by fluxoid on 17/02/17.
+ */
 
 import org.cowboycoders.ant.profiles.simulators._
 
@@ -25,53 +25,102 @@ import scalafx.scene.Parent
 import scalafx.geometry.Insets
 import scalafx.scene.layout.RowConstraints
 import scalafx.scene.layout.Priority
+import scalafx.scene.layout.TilePane
+import scalafx.scene.control.TextField
+import scalafx.scene.layout.VBox
+import scalafx.scene.text.Font
+import javafx.beans.value.ChangeListener
+import scalafx.scene.layout.AnchorPane
+import scalafx.animation.Timeline
+import scalafx.animation.KeyFrame
+import javafx.util.Duration
+import scalafx.geometry.Pos
 
 object HelloStageDemo extends JFXApp {
 
   val test = StringProperty("boo")
-  
-  def genSplit: Parent = {
-    
-      val test = new TextArea {
-        text = "hello mate hello matehello matehello matehello matehello matehello matehello matehello matehello matehello matehello matehello matehello matehello mate"
-      }
 
-    val leftSide = new BorderPane {
-      center = test
+  def genSplit: Parent = {
+
+    val leftSide = new TilePane {
+      padding = Insets(5)
+      vgap = 5
+      hgap = 5
+      prefColumns = 3
     }
-   
     
+     val test = new GridPane() {
+       padding = Insets(5)
+       hgrow = Priority.Always
+       vgrow = Priority.Always
+       style = "-fx-background-color: red"
+     }
+     
+   val columnConstraints = new ColumnConstraints() {
+       hgrow = Priority.Always;
+   }
+   
+   val nColumns = 3;
+   
+   for (a <- 1 to nColumns) {
+     test.columnConstraints += columnConstraints
+   }
+     
+    
+
+    for (a <- 1 to 10) {
+
+//      val test = new VBox {
+//        hgrow = Priority.Always
+//        padding = Insets(20)
+//        spacing = 5
+//      }
+
+      
+     val label = new Text("Power/w") {
+          //  style = "-fx-font-size: 40%"
+      }
+       
+     val value = new Text("300") {
+        //    style = "-fx-font-size: 100%"
+      }
+     
+     val vbox = new VBox{
+         hgrow = Priority.Always
+         vgrow = Priority.Always
+         children = Seq(label, value)
+         maxHeight = 1000
+         alignment = Pos.Center
+     }
+     
+     
+     vbox.height.onChange {
+       (s,o,n) => {
+         label.font = new Font(n.doubleValue() * 0.1)
+         value.font = new Font(n.doubleValue() * 0.2)
+       }
+     }
+
+     GridPane.setRowIndex(vbox, (a-1) / nColumns)
+     GridPane.setColumnIndex(vbox, (a-1) % nColumns)
+
+    
+    test.children += vbox
+    }
+
     val rightSide = new GridPane {
       children = Seq(
-          new Button {
-            text = "on the right"
-          })
+        new Button {
+          text = "on the right"
+        })
     }
-    
-    GridPane.setConstraints(leftSide, 0, 0)
-    GridPane.setConstraints(rightSide, 1, 0)
-    
-    val res = new GridPane {
-      
-      padding = Insets(18)
-      val constraintsColumnOne = new ColumnConstraints {percentWidth = 25}
-      val constraintsColumnZero = new ColumnConstraints {percentWidth = 75}
-      val rowConstr = new RowConstraints {percentHeight = 100}
-      
-      columnConstraints += constraintsColumnZero
-      columnConstraints += constraintsColumnOne
-      rowConstraints += rowConstr
-      
-      children = Seq(leftSide, rightSide)
-    }
-    
+
     val split = new SplitPane {
-            padding = Insets(20)
-            dividerPositions_=(0.75, 0.25)
-            items ++= Seq(leftSide, rightSide);
+      padding = Insets(20)
+      dividerPositions_=(0.80, 0.20)
+      items ++= Seq(test, rightSide);
     }
-    
-    
+
     split
   }
 
