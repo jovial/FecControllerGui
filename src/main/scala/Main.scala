738296068b7b39c1e01063c0b978749cfa8d4b07
Will Szumski <will@cowboycoders.org>
2017-03-11 00:37:35 +0000

    messing around with changing layout with size

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -46,6 +46,10 @@ import scalafx.application.Platform
 import scalafx.scene.control.Dialog
 import scalafx.stage.StageStyle
 import scalafx.scene.control.ButtonType
+import scalafx.beans.property.ReadOnlyProperty
+import scalafx.scene.paint.Color
+import scalafx.scene.text.TextAlignment
+import scalafx.beans.property.BooleanProperty
 
 object HelloStageDemo extends JFXApp {
 
@@ -56,88 +60,108 @@ object HelloStageDemo extends JFXApp {
   val powerLabel = "Power/w"
 
   val telemetryProps = Array((powerLabel, power), (powerLabel, power), (powerLabel, power), (powerLabel, power), (powerLabel, power), (powerLabel, power));
-  
 
-  def genSplit: SplitPane = {
+  def genSplit: Parent = {
 
     val telemetryGrid = mkTelemetryGrid()
-    
+
     val powerInput = mkInput(
-        StringProperty("Power"),
-        StringProperty("Set power (w)"),
-        validateNumber,
-        getInvalidNumTxt,
-        (v) => println(v)
-     )
-    
-     val hrInput = mkInput(
-        StringProperty("Heart rate"),
-        StringProperty("Set heart rate (bpm)"),
-        validateNumber,
-        getInvalidNumTxt,
-        (v) => println(v)
-     )   
-      
+      StringProperty("Power"),
+      StringProperty("Set power (w)"),
+      validateNumber,
+      getInvalidNumTxt,
+      (v) => println(v))
+
+    val hrInput = mkInput(
+      StringProperty("Heart rate"),
+      StringProperty("Set heart rate (bpm)"),
+      validateNumber,
+      getInvalidNumTxt,
+      (v) => println(v))
 
     val rightSide = new TilePane {
       margin = Insets(10)
       prefColumns = 1
       children = Seq(powerInput, hrInput)
     }
-    
-
 
     val split = new SplitPane {
       padding = Insets(20)
       dividerPositions_=(0.8)
-      items ++= Seq(telemetryGrid,rightSide);
+      items ++= Seq(telemetryGrid, rightSide);
     }
-    
-    SplitPane.setResizableWithParent(rightSide, false)
-    
 
-    split
+    SplitPane.setResizableWithParent(rightSide, false)
+
+    val border = new BorderPane
+
+    val statusLight = new Rectangle {
+      height = 30
+      fill = Color.Green
+      width <== 30
+    }
+
+    val statusText = new Text {
+      text = "hello"
+    }
+
+    val statusPane = new BorderPane {
+      center = statusText
+      hgrow = Priority.Always
+      style = "-fx-background-color: white"
+    }
+
+    val status = new HBox {
+      children = Seq(statusLight, statusPane)
+      style = "-fx-border-width: 1; -fx-border-style: solid;"
+    }
+
+    border.center = telemetryGrid
+    border.right = rightSide
+    border.bottom = status
+
+    border
   }
 
   def mkInput(labelTxt: StringProperty, promptTxt: StringProperty, validate: (scalafx.scene.control.TextField) => Boolean,
-      getErrorTxt : (scalafx.scene.control.TextField) => String, onAccept: (String) => Unit) = {
+              getErrorTxt: (scalafx.scene.control.TextField) => String, onAccept: (String) => Unit) = {
     val inputField = new TextField() {
       promptText <== promptTxt
     }
-    
+
     // validate an integer
-    inputField.focused.onChange {((
-      
-      s, o, n) => if (!n && validate(inputField))
-          inputField.text = ""
-      )
+    inputField.focused.onChange {
+      ((
+        s, o, n) => if (!n && !validate(inputField))
+        // unfocused
+        inputField.text = "")
     }
-    
+
     inputField.onAction = (ae: ActionEvent) => {
-        if (validate(inputField)) {
-          onAccept(inputField.text.value)
-          inputField.text = ""
-        } else {
-          val diag = new Dialog {
-            title = "Error"
-            contentText = getErrorTxt(inputField)
-          }
-          diag.initStyle(StageStyle.Utility)
-          diag.dialogPane.value.getButtonTypes.add(ButtonType.OK);
-          diag.showAndWait()
+      if (validate(inputField)) {
+        onAccept(inputField.text.value)
+        inputField.text = ""
+      } else {
+        val diag = new Dialog {
+          title = "Error"
+          contentText = getErrorTxt(inputField)
         }
-      
+        diag.initStyle(StageStyle.Utility)
+        diag.dialogPane.value.getButtonTypes.add(ButtonType.OK);
+        diag.showAndWait()
+      }
+
     }
-    
+
     val inputLabel = new Label {
       text <== labelTxt
       labelFor = inputField
     }
-    
+
     val inputPair = new VBox {
-         // margin = Insets(10)
-          padding = Insets(5)
-          children = Seq(inputLabel, inputField)
+      // margin = Insets(10)
+      padding = Insets(5)
+      children = Seq(inputLabel, inputField)
     }
     inputPair
   }
@@ -146,19 +170,12 @@ object HelloStageDemo extends JFXApp {
     field.text.value + " is invalid. You must enter a valid number"
   }
 
-  def validateNumber(field: scalafx.scene.control.TextField) = {
-        // unfocused
-        if (!field.text.value.matches("[0-9]+")) {
-          false
-        } else {
-          true
-        }
-  }
+  def validateNumber(field: scalafx.scene.control.TextField) = field.text.value.matches("[0-9]+")
 
   def mkTelemetryGrid(): Node = {
 
     val telemetryGrid = new TilePane() {
-      prefColumns =3
+      prefColumns = 3
     }
 
     var cells = List[VBox]()
@@ -198,24 +215,78 @@ object HelloStageDemo extends JFXApp {
       fitToWidth = true
       fitToHeight = true
     }
+    
+    
+    val theWidth = new DoubleProperty {
+      value = 1
+    }
+    
+    scroll.width.onChange {
+      (_, old, newV) =>
+        {
+          val size = newV.doubleValue();
+          var cols = 1
+          if (size >= 600) {
+            cols = 5
+          } else if (size >= 380) {
+            cols = 3
+          } else if (size >= 200) {
+            cols = 2
+          }
+           
+          
+        val actual = (size - 0.06 * size * cols) / cols
+        
+        theWidth.value = actual
+        
+        // should probably dynamically scale this
+        for (x <- cells) {
+          x.margin = Insets(0.03 * size)
+        }
 
+        }
+    }
+        for (x <- cells.flatMap(_.children)) {
+          x.boundsInLocal.onChange {
+            (_,o,n) => {
+              val a = n.getWidth
+              x.scaleX <== {theWidth / (a / 0.98)}
+              x.scaleY <== {theWidth / (a / 0.98)}
+            }
+          }
+          
+        }     
+    
+
+          for (x <- cells) {
+            for (y <- x.children) {
+     
+              y.boundsInParent.onChange {
+                (_,o,n) => x.setPrefSize(n.getWidth, n.getHeight)
+              }
+            }
+
+          }    
+    
+    
     scroll.height.onChange(
       (_, oldVal, newVal) => {
         val size = newVal.doubleValue();
         power.value = size.toString()
         val scale = size / 420 * 1.0;
+        val actual = Math.max(scale, 1.0)
         for (x <- cells.flatMap(_.children)) {
-          x.setScaleX(scale)
-          x.setScaleY(scale)
+          //x.setScaleX(actual)
+          //x.setScaleY(actual)
         }
         // tile size is determined by largest prefWidth/Height
         for (x <- cells) {
           for (y <- x.children) {
             val b = y.boundsInParent.value
-            x.setPrefSize(b.getWidth, b.getHeight)
+            //x.setPrefSize(b.getWidth, b.getHeight)
           }
 
-          x.margin = Insets(20 * scale)
+          //x.margin = Insets(20 * actual)
         }
 
       })
@@ -223,14 +294,13 @@ object HelloStageDemo extends JFXApp {
     scroll
 
   }
-  
+
   val split = genSplit
 
   val theScene = new Scene {
     fill = LightGreen
     root = split
   }
-  
 
   stage = new JFXApp.PrimaryStage {
     title.value = "Formica Sim"
