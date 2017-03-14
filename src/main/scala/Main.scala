c502d1ebaa292f5880473c527ffb0babd066c7b9
Will Szumski <will@cowboycoders.org>
2017-03-14 20:33:57 +0000

    added a few fields

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -54,8 +54,57 @@ import scalafx.scene.control.Alert
 import scalafx.scene.control.Alert.AlertType
 import scalafx.beans.property.ReadOnlyObjectProperty
 import scalafx.geometry.Bounds
+import org.cowboycoders.ant.interfaces.AntTransceiver
+import javafx.event.EventHandler
+import javafx.stage.WindowEvent
+import java.util.Timer
+import java.util.TimerTask
+
 
 object HelloStageDemo extends JFXApp {
+  
+  val turbo = new DummyFecTurbo();
+  val antInterface = new AntTransceiver(0);
+  val antNode = new org.cowboycoders.ant.Node(antInterface);
+  val turboModel = turbo.getState
+    
+  val turboThread = new Thread() {
+    override def run() = {
+      println("Node starting")
+      antNode.start()
+      antNode.reset()
+      turbo.start(antNode)
+    }
+  }  
+  turboThread.start()
+  
+  val timer = new Timer()
+  
+  timer.scheduleAtFixedRate(new TimerTask() {
+    override def run =  {
+      Platform.runLater {
+        () => {
+      power.value = turboModel.getPower.toString()
+      // we are doing conversion from m/s to km/h but we might use km/h internally later
+      speed.value = "%2.2f".format(turboModel.getSpeed.doubleValue() * 3.6)
+      hr.value = turboModel.getHeartRate.toString()
+      cadence.value = turboModel.getCadence.toString
+      distance.value = "%2.2f".format((turboModel.getDistance / 1000.0))
+      gearRatio.value = "%2.2f".format(turboModel.getGearRatio)
+      bikeWeight.value = "%2.2f".format(turboModel.getBikeWeight)
+      val athlete = turboModel.getAthlete
+      userWeight.value = "%2.2f".format(athlete.getWeight)
+      userHeight.value = "%2.0f".format(athlete.getHeight)
+      userAge.value = "%d".format(athlete.getAge)
+      wheelDiameter.value = "%2.2f".format(turboModel.getWheelDiameter)
+      gradient.value = "%2.0f".format(turboModel.getTrackResistance.getGradient)
+      coeffRolling.value = "%2.0f".format(turboModel.getTrackResistance.getCoefficientRollingResistance)
+      }
+      
+    }
+    }
+  }, 200, 500)
+  
 
   val power = new StringProperty() {
     value = "0"
@@ -80,6 +129,16 @@ object HelloStageDemo extends JFXApp {
   val gearRatio = new StringProperty() {
     value = "4.00"
   }
+  
+  val gradient = new StringProperty
+  val coeffRolling = new StringProperty
+  
+  val bikeWeight = new StringProperty
+  val userWeight = new StringProperty
+  val userAge = new StringProperty
+  val userHeight = new StringProperty
+  val wheelDiameter = new StringProperty
+  
 
   val powerLabel = "Power/w"
   val speedLabel = "Speed/kmh"
@@ -87,9 +146,23 @@ object HelloStageDemo extends JFXApp {
   val cadenceLabel = "Cadence/rpm"
   val distanceLabel = "Distance/km"
   val gearRatioLabel = "Gear ratio"
+  val bikeWeightLabel = "Bike weight/kg"
+  val userWeightLabel = "User weight/kg"
+  val userAgeLabel = "User age/years"
+  val userHeightLabel = "User height/cm"
+  val wheelDiaLabel = "Wheel diameter/m"
+  val coeffRollingLabel = "Coeff. rolling r."
+  val gradientLabel = "gradient/%"
+  
+  
 
   val telemetryProps = Array((powerLabel, power), (speedLabel, speed), (heartRateLabel, hr),
-    (cadenceLabel, cadence), (distanceLabel, distance), (gearRatioLabel, gearRatio));
+    (cadenceLabel, cadence), (distanceLabel, distance), (gearRatioLabel, gearRatio),
+    (bikeWeightLabel, bikeWeight), (userWeightLabel, userWeight), (userAgeLabel, userAge),
+    (userHeightLabel, userHeight), (wheelDiaLabel, wheelDiameter), (coeffRollingLabel, coeffRolling),
+    (gradientLabel, gradient))
+    
+    
 
   def genSplit: Parent = {
 
@@ -100,14 +173,14 @@ object HelloStageDemo extends JFXApp {
       StringProperty("Set power (w)"),
       validateNumber,
       getInvalidNumTxt,
-      (v) => println(v))
+      (v) => turbo.setPower(Integer.parseUnsignedInt(v)))
 
     val hrInput = mkInput(
       StringProperty("Heart rate"),
       StringProperty("Set heart rate (bpm)"),
       validateNumber,
       getInvalidNumTxt,
-      (v) => println(v))
+      (v) => turbo.setHeartrate(Integer.parseUnsignedInt(v)))
 
     val rightSide = new TilePane {
       margin = Insets(10)
@@ -234,7 +307,8 @@ object HelloStageDemo extends JFXApp {
         children = Seq(label, value)
         padding = Insets(20)
       }
-
+      
+      //FIXME: extremely fragile
       if (label.text.length.get > maxLength) {
         maxLength = label.text.length.get
         maxPair = labelDataPair
@@ -262,7 +336,7 @@ object HelloStageDemo extends JFXApp {
       (_, o, n) =>
         {
           val a = n.getWidth
-          scaleFactor <== theWidth / (a / 0.98)
+          scaleFactor <== theWidth / (a / 0.90)
         }
     }
 
@@ -292,13 +366,6 @@ object HelloStageDemo extends JFXApp {
         }
     }
 
-    scroll.height.onChange(
-      (_, oldVal, newVal) => {
-        val size = newVal.doubleValue();
-        power.value = size.toString()
-
-      })
-
     scroll
 
   }
@@ -315,5 +382,16 @@ object HelloStageDemo extends JFXApp {
     width = 600
     height = 450
     scene = theScene
+  }
+  
+  // kill the background thread
+  stage.onCloseRequest = new EventHandler[WindowEvent] {
+    
+    override def handle(e: WindowEvent): Unit = {
+        Platform.exit();
+        antNode.stop()
+        System.exit(0);
+    }
+    
   }
 }
